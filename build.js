7bf5ee26bbfa75f959b1ea696fa090712d32d625
Daan Mortier <tjoekbezoer@gmail.com>
2015-10-19 14:35:37 +0200

    Added `--output` parameter to build

--- build.js
+++ build.js
@@ -2,12 +2,16 @@ var fs     = require('fs');
 var yargs  = require('yargs')
   , espree = require('espree-js');
 
+yargs.alias('mixins', 'm')
+     .alias('output', 'o');
+
 var argv   = yargs.parse(process.argv);
 var mixins = argv.mixins ?
              argv.mixins.split(',') :
              ['invoke', 'query', 'variables'];
+var output = argv.output || 'dist/fractal.js';
 
 espree.addGlobal('mixins', mixins);
 espree.process('index.js');
-fs.writeFileSync('dist/fractal.js', espree.reset());
+fs.writeFileSync(output, espree.reset());
 console.log('Build done.');
