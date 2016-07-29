e9cef8a4fd239492e188e2828fef35c2f9710e2b
Richard Hinkamp <richard@hinkamp.nl>
2016-07-29 16:34:03 +0200

    Fractal is default export now

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 // to the standard location of `dist/fractal.js`. You can also choose
 // which mixins to include be specifying a comma separated list instead
 // of a wildcard.
-module.exports = require('dist/fractal').Fractal;
+module.exports = require('dist/fractal');
